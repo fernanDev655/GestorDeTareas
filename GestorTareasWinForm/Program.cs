329b25e79d57d6a9d23ebdd3279628e608f6e51b
fernanDev655 <fergallop@alu.edu.gva.es>
2026-03-29 16:16:12 +0200

    añadida funcionalidad del login

--- GestorTareasWinForm/Program.cs
+++ GestorTareasWinForm/Program.cs
@@ -12,6 +12,8 @@ namespace GestorTareasWinForm
 			// see https://aka.ms/applicationconfiguration.
 			ApplicationConfiguration.Initialize();
 			Application.Run(new Form1());
+
+			Console.WriteLine("login añadido");
 		}
 	}
 }
